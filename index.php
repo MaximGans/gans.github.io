3ef679a287a6c236d7b87d3354ea925488d1351c
MaximGans <73780037+MaximGans@users.noreply.github.com>
2021-05-23 21:14:25 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+﻿<?php
+$money = 0;
+$fish = 0;
+
+
+?>
+<html>
+<head>
+	<link href="css/style.css" type="text/css" rel="stylesheet" />
+
+
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+</head>
+
+
+<body>
+
+
+	<header>
+
+		<div id="header"><div id="logo" style="width:100%;height:50px;background-color:aqua;"><p>Money</p></div></div>
+
+
+	</header>
+	<img src="img/fish.png" id="fish" />
+
+
+	<div id='score'>
+
+		<sc id="money">Деньги:</sc> <money>0</money><br />
+
+		<sc> Килька: </sc><fish id="kilka">0</fish><br />
+		<sc> Тунец:  </sc><fish id="tynec">0</fish><br />
+		<sc> Треска:  </sc><fish id="treska">0</fish><br />
+		<sc> Семга:  </sc><fish id="semga">0</fish><br />
+		<sc> Окунь:  </sc><fish id="okyn">0</fish><br />
+
+
+		<up1>up1</up1><br />
+
+
+
+
+		<sell>Продать</sell>
+		<sell5>Продать x5</sell5>
+		<sell10>Продать x10</sell10>
+
+
+		<p></p>
+		<p></p>
+		<p></p>
+
+
+	</div>
+
+
+
+<script>
+    $(document).ready(function () {
+        var cena = 50;
+        var money = 0;
+        /*cena*/
+        var kilca_cena = 50;
+        var tynec_cena = 50;
+        var treska_cena = 50;
+        var semga_cena = 50;
+        var okyn_cena = 50;
+
+        /*cena*/
+        /*fish*/
+        var kilca = 0;
+        var tynec = 0;
+        var treska = 0;
+        var semga = 0;
+        var okyn = 0;
+        /*fish click*/
+        var kilca_click = 1;
+        var tynec_click = 1;
+        var treska_click = 1;
+        var semga_click = 1;
+        var okyn_click = 1;
+        /*fish click*/
+
+        $("#fish").click(function () {
+            kilca += kilca_click;
+            $('#kilka').text(kilca);
+
+        });
+
+        $("sell").click(function () {
+            if (kilca > 0) {
+
+                kilca -= 1;
+                money += 5;
+                $('money').text(money);
+                $('#kilka').text(kilca);
+            }
+
+
+        });
+
+
+        $("sell5").click(function () {
+            if (kilca >= 5) {
+
+                kilca -= 5;
+                money += 25;
+                $('money').text(money);
+                $('#kilka').text(kilca);
+            }
+
+
+        });
+
+        $("sell10").click(function () {
+            if (kilca >= 10) {
+
+                kilca -= 10;
+                money += 50;
+                $('money').text(money);
+                $('#kilka').text(kilca);
+            }
+
+
+        });
+
+        /*$("up1").click(function () {
+            if (money >=cena){
+
+                fishcl *=2;
+                money -=cena;
+                cena *=2;
+                $('money').text(money);
+                $('fish').text(fish);
+            }
+
+
+        });*/
+
+
+    });
+</script>
+
+</body>
+
+
+
+
+</html>
